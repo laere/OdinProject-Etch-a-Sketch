54950e738964459d238120cfebbd2b9dc630c0b6
Zack <bostianz@gmail.com>
2023-09-23 11:57:49 -0400

    able to create and add divs based on user input

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 //When clicked it will prompt the user for the # of sides for the new grid (100x100)
 
 let button = document.getElementById("new-sketch-btn");
-
+let container = document.getElementById("container");
 
 function grid(n) {
   
-    let container = document.getElementById("container");
+   
     console.log(container)
 
     if (n > 100 || n === 0 || n === NaN) {
         return;
     }
 
-    for (let i = 0; i >= n; i ++) {
+    for (let i = 0; i < n; i++) {
         let box = document.createElement("div")
         box.className = "box"
         container.appendChild(box);
